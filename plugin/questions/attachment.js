35db7cbecfa2a2fdb314b184c0a77a66b546728d
Melissa Mcewen <melissa@equivoice.com>
2015-06-11 00:28:41 -0500

    new category bassed on the book Attached

--- plugin/questions/attachment.js
+++ plugin/questions/attachment.js
@@ -21,9 +21,35 @@ _OKCP.fileQuestions.attachment =
         },
         {
             "qid":"19236",
-            "text":"Is there a point in a relationship where it can get \"too serious\"?",
+            "text":"Is there a point in a relationship where it can get &quot;too serious&quot;?",
+            "answerText": ["Yes", "No"],
+            "score": [-1, 1]
+        },
+
+        {
+            "qid":"491",
+            "text":"What would be more likely to bother you in a relationship?",
+            "answerText": ["Not enough space", "Not enough affection"],
+            "score": [-1, 1]
+        },
+        {
+            "qid":"20135",
+            "text":"How do you feel about falling in love?",
+            "answerText": ["I love it and want it very much", "I try to avoid it", "I like to just let it happen", "I'm indifferent / not sure"],
+            "score": [1, -1, 1, -1],
+            "weight":[1,1,1,0.5]
+        },
+        {
+            "qid":"54016",
+            "text":"Are you afraid of falling madly in love?",
             "answerText": ["Yes.", "No."],
             "score": [-1, 1]
+        },
+        {
+            "qid":"490",
+            "text":"Serious relationships feel...",
+            "answerText": ["...Great!", "...Like jail"],
+            "score": [1, -1]
         }
 
     ],
@@ -32,6 +58,18 @@ _OKCP.fileQuestions.attachment =
         {
             "qid":"32521",
             "text":"Do you ever date to avoid being alone?",
+            "answerText": ["Yes.", "No."],
+            "score": [-1, 1]
+        },
+        {
+            "qid":"60500",
+            "text":"Has anyone ever accused you of being &quot;too clingy&quot;?",
+            "answerText": ["Yes.", "No."],
+            "score": [-1, 1]
+        },
+        {
+            "qid":"61170",
+            "text":"Has anyone ever accused you of being &quot;high maintenance&quot;?",
             "answerText": ["Yes.", "No."],
             "score": [-1, 1]
         }
