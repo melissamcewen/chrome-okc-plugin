26c8d09f40c7180854394b7142222c7861c28aba
Melissa Mcewen <melissa@equivoice.com>
2015-06-10 21:28:38 -0500

    new category bassed on the book Attached

--- plugin/questions/attachment.js
+++ plugin/questions/attachment.js
@@ -4,30 +4,26 @@ _OKCP.fileQuestions.attachment =
         {
             "qid":"1420",
             "text":"Are you going to run away as soon as it is commitment time?",
-            "answerText": ["Yes.", "No."],
-            "score": [1, -1],
-            "weight": [1, 1]
+            "answerText": ["Yes", "No"],
+            "score": [-1, 1]
         },
         {
             "qid":"4043",
             "text":"Are you scared of commitment?",
             "answerText": ["Yes.", "No."],
-            "score": [1, -1],
-            "weight": [1,1]
+            "score": [-1, 1]
         },
         {
             "qid":"23233",
             "text":"Do you have time for a committed relationship in your life at this point?",
             "answerText": ["Yes, absolutely", "Probably not", "No"],
-            "score": [-1, 1, 1],
-            "weight":[1,0.5,1]
+            "score": [1, -1, -1]
         },
         {
             "qid":"19236",
             "text":"Is there a point in a relationship where it can get \"too serious\"?",
             "answerText": ["Yes.", "No."],
-            "score": [1, -1],
-            "weight": [1, 1]
+            "score": [-1, 1]
         }
 
     ],
@@ -37,8 +33,7 @@ _OKCP.fileQuestions.attachment =
             "qid":"32521",
             "text":"Do you ever date to avoid being alone?",
             "answerText": ["Yes.", "No."],
-            "score": [1, -1],
-            "weight": [1,1]
+            "score": [-1, 1]
         }
     ]
 
